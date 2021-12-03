f6883965da6f3de1e076ff3d959adfaea82e1fe8
antoniosalvia <antonio.salvia@mail.de>
2021-12-03 14:51:38 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,21 @@
+'use strict';
+
+let film = [
+    [,],
+    [,],
+    [,],
+];
+
+
+film[0][0]= prompt('Geben Sie ersten Ihren Filmtitel ein:');
+film[0][1] = prompt('Geben Sie das Veröffentlichungsjahr des ersten Filmes ein:');
+
+film[1][0] = prompt('Geben Sie Ihren zweiten Filmtitel ein:');
+film[1][1] = prompt('Geben Sie das Veröffentlichungsjahr des Filmes ein:');
+
+film[2][0] = prompt('Geben Sie Ihren dritten Filmtitel ein:');
+film[2][1] = prompt('Geben Sie das Veröffentlichungsjahr des Filmes ein:');
+
+for (let i = 0; i < film.length; i++) {
+    console.log(film[i]);
+};
